45cd057142ad0b794369291ab9870d4f0b5d2577
Chris <creedrogers@gmail.com>
2026-01-03 10:51:20 -0500

    update

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
-gotta have:
+### dependencies
+cmake
+gcc or g++ at least 17 (don't want 17?  change line 4 of root level CMakeLists.txt)
+raylib 5.5 (don't want 5.5?  change line 12 of CMakeLists.txt)
 
-cmake<br>
-g++<br>
-vscode<br>
-
-put your code in src<br>
-put your resources in res
-
-mkdir _bld<br>
-cd _bld<br>
-cmake ..<br>
-make<br>
-./app
+mkdir build
+cd build
+cmake ..
+make
+cd raylib-fun  # this folder is created because of lines 17-19 in the root level CMakeLists.txt
+./raylib-fun   # executable name will be PROJECT_NAME, which will be project(raylib-fun), from root level CMakeLists.txt
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "you are here\n";
+#include "raylib.h"
+
+const bool DEBUG_GAME = false;
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+
+int main(void) {
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game!");
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        float dt = GetFrameTime();
+        std::cout << "Delta time: " << dt << "\n";
+
+        BeginDrawing();
+            ClearBackground(BLACK);
+            DrawRectangle(100, 100, 200, 300, BLUE);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
